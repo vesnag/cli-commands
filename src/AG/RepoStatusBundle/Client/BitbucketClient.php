4bb969fd6186c154e5df20af3840d292c01840ba
Vesna Groselj <vgroselj@gmail.com>
2024-08-28 17:32:49 +0200

    Cleanup

--- src/AG/RepoStatusBundle/Client/BitbucketClient.php
+++ src/AG/RepoStatusBundle/Client/BitbucketClient.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 namespace AG\RepoStatusBundle\Client;
 
-use AG\RepoStatusBundle\Config\GitHubConfig;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-
 class BitbucketClient implements VcsClient
 {
     public function getPullRequests(): array
     {
-        // Dummy implementation
         return [];
     }
 }
--- src/AG/RepoStatusBundle/Client/GitLabClient.php
+++ src/AG/RepoStatusBundle/Client/GitLabClient.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace AG\RepoStatusBundle\Client;
 
-use AG\RepoStatusBundle\Config\GitHubConfig;
-use AG\RepoStatusBundle\Model\PullRequest;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-
 class GitLabClient implements VcsClient
 {
     public function getPullRequests(): array
