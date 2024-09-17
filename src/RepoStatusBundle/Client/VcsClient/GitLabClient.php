eabe3c251049c2c9873e9e1b0b3cdceacc146e35
Vesna Groselj <vgroselj@gmail.com>
2024-09-17 14:14:36 +0200

    Add remaining methods

--- src/RepoStatusBundle/Client/VcsClient/GitLabClient.php
+++ src/RepoStatusBundle/Client/VcsClient/GitLabClient.php
@@ -4,10 +4,41 @@ declare(strict_types=1);
 
 namespace App\RepoStatusBundle\Client\VcsClient;
 
+use App\RepoStatusBundle\DTO\GitHubQueryParams;
+use App\RepoStatusBundle\Model\Commit;
+use App\RepoStatusBundle\Model\PullRequest;
+
 class GitLabClient implements VcsClient
 {
-    public function getPullRequests(): array
+    /**
+     * @inheritDoc
+     */
+    public function getPullRequests(?GitHubQueryParams $queryParams = null): array
     {
         return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCommits(?GitHubQueryParams $queryParams = null): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPullRequestsCount(?GitHubQueryParams $queryParams = null): int
+    {
+        return 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCommitsCount(?GitHubQueryParams $queryParams = null): int
+    {
+        return 0;
     }
 }
--- src/RepoStatusBundle/Client/VcsClient/VcsClient.php
+++ src/RepoStatusBundle/Client/VcsClient/VcsClient.php
@@ -4,12 +4,33 @@ declare(strict_types=1);
 
 namespace App\RepoStatusBundle\Client\VcsClient;
 
+use App\RepoStatusBundle\DTO\GitHubQueryParams;
+use App\RepoStatusBundle\Model\Commit;
 use App\RepoStatusBundle\Model\PullRequest;
 
 interface VcsClient
 {
     /**
+     * @param GitHubQueryParams|null $queryParams
      * @return PullRequest[]
      */
-    public function getPullRequests(): array;
+    public function getPullRequests(?GitHubQueryParams $queryParams = null): array;
+
+    /**
+     * @param GitHubQueryParams|null $queryParams
+     * @return Commit[]
+     */
+    public function getCommits(?GitHubQueryParams $queryParams = null): array;
+
+    /**
+     * @param GitHubQueryParams|null $queryParams
+     * @return int
+     */
+    public function getPullRequestsCount(?GitHubQueryParams $queryParams = null): int;
+
+    /**
+     * @param GitHubQueryParams|null $queryParams
+     * @return int
+     */
+    public function getCommitsCount(?GitHubQueryParams $queryParams = null): int;
 }
